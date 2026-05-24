2e78f41850796202ccc3e4fcd306df3aca8da5a7
Ashot Mirzoyan <ashotmirzoan@gmail.com>
2026-05-24 16:29:27 +0300

    fix

--- windows.py
+++ windows.py
@@ -1,5 +1,10 @@
+import platform
+import sys
+
 import host_guard
 
-
 if __name__ == "__main__":
+    if platform.system() != "Windows":
+        print("Этот файл предназначен для запуска только в Windows. Используй macos.py.")
+        sys.exit(1)
     host_guard.run("windows.enc")
